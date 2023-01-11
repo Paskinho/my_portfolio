78296711204299a38f2837564a1636b85663d1b9
paskanov <v.paskanov@gmail.com>
2023-01-11 17:14:50 +0300

    some changed in css nav and header

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -4,11 +4,10 @@ import s from "./Nav.module.css"
  export const Nav = () => {
     return(
 <div className={s.nav}>
-     <a href=''></a>
-     <a href=''></a>
-     <a href=''></a>
-     <a href=''></a>
-     <a href=''></a>
+     <a href=''>Home</a>
+     <a href=''>Skills</a>
+     <a href=''>Projects</a>
+     <a href=''>Contact us</a>
 </div>
     );
 }
