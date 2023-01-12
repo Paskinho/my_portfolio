436dd5c5e140290420cbd6d0ab3daa87ae9693f5
paskanov <v.paskanov@gmail.com>
2023-01-12 10:50:18 +0300

    Add main

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import {Header} from "./components/header/Header";
+import { Main } from './components/main/Main';
 
 function App() {
   return (
     <div className="App">
 <Header/>
+        <Main/>
     </div>
   );
 }
---
+++ src/components/main/Main.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import s from "./Main.module.css"
+
+
+export const Main = () => {
+    return(
+        <div className={s.mainBlock}>
+
+        </div>
+    );
+}
