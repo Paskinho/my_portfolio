d70f5c6ea8178996a866935ec9cb4a9dd7e46d16
paskanov <v.paskanov@gmail.com>
2023-01-12 12:11:06 +0300

    add Container.module.css

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import s from "./Main.module.css"
+import sContainer from '../../common/styles/Container.module.css'
 
 
 export const Main = () => {
     return (
         <div className={s.mainBlock}>
-            <div className={s.container}>
+            <div className={sContainer.container}>
             <div className={s.text}>
                 <span>Hello!</span>
                 <h1>I am Uladzislau Paskanau</h1>
