7c7a16c82f7d7338e27a1decfb80b939e387792b
paskanov <v.paskanov@gmail.com>
2023-01-12 12:45:06 +0300

    changed css in skills

--- src/components/skills/Skills.tsx
+++ src/components/skills/Skills.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import s from "./Skills.module.css"
-
+import sContainer from '../../common/styles/Container.module.css'
 
 
 export const Skills = () => {
-    return(
+    return (
         <div className={s.skillsBlock}>
+            <div className={`${sContainer.container} ${s.skillsContainer}`}>
+                <h2 className={s.title}>Skills</h2>
+                <div className={s.skills}></div>
 
+            </div>
         </div>
     );
 }
