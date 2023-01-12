a1ed4113ebffa21b8fc17d06b7028a668025b19e
paskanov <v.paskanov@gmail.com>
2023-01-12 14:39:19 +0300

    changed css in skills

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import './App.css';
 import {Header} from "./components/header/Header";
-import { Main } from './components/main/Main';
+import {Main} from './components/main/Main';
+import {Skills} from "./components/skills/Skills";
 
 function App() {
-  return (
-    <div className="App">
-<Header/>
-        <Main/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <Main/>
+            <Skills/>
+        </div>
+    );
 }
 
 export default App;
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import s from "./Header.module.css"
 import {Nav} from "../nav/Nav";
 
- export const Header = () => {
-    return(
-<div className={s.header}>
-<Nav/>
-</div>
+export const Header = () => {
+    return (
+        <div className={s.header}>
+            <Nav/>
+        </div>
     );
 }
