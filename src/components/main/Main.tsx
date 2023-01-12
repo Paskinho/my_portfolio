58bb1e8461b339dfe9bf3c8d006946de66ada659
paskanov <v.paskanov@gmail.com>
2023-01-12 11:51:20 +0300

    changed css in Main.tsx

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -3,9 +3,18 @@ import s from "./Main.module.css"
 
 
 export const Main = () => {
-    return(
+    return (
         <div className={s.mainBlock}>
-
+            <div className={s.container}>
+            <div className={s.text}>
+                <span>Hello!</span>
+                <h1>I am Uladzislau Paskanau</h1>
+                <p>Frontend Developer</p>
+            </div>
+            <div className={s.photo}>
+                <img src=""/>
+            </div>
+            </div>
         </div>
     );
 }
