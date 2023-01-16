7976655f579aaf021fb95a3dd2c4e0af5c64de3b
paskanov <v.paskanov@gmail.com>
2023-01-16 12:52:52 +0300

    Refactoring Footer.tsx

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -5,7 +5,9 @@ import sContainer from "../../common/styles/Container.module.css";
 export const Footer = () => {
     return(
         <div className={s.footerBlock}>
-
+            <div className={sContainer.container}>
+            <h2 className={s.name}>Uladzislau Paskanau </h2>
+            </div>
         </div>
     );
 }
