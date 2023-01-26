b61423d23dd0dd941968a3becf3dcfbb85370399
paskanov <v.paskanov@gmail.com>
2023-01-26 17:02:11 +0300

    some refactoring Projects.module.css

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -8,14 +8,7 @@ export const Projects = () => {
     return (
         <div className={s.mainBlock}>
             <div className={sContainer.container}>
-            <div className={s.text}>
-                <span>Hello!</span>
-                <h1>I am Uladzislau Paskanau</h1>
-                <p>Frontend Developer</p>
-            </div>
-            <div className={s.photo}>
-                <img src=''/>
-            </div>
+                <h2 className={s.title}>Projects</h2>
             </div>
         </div>
     );
