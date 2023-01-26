829980f85437b272a1804f3d3b362d9c2d20f7b0
paskanov <v.paskanov@gmail.com>
2023-01-26 09:49:48 +0300

    some changes in Footer.tsx and Footer.module.css

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -8,6 +8,13 @@ export const Footer = () => {
             <div className={`${sContainer.container} ${s.footerContainer}`}>
                 <div className={s.name}>
             <span>Uladzislau Paskanau</span>
+                    <div>
+                        <div className={s.item}><img alt={"Insta"}/></div>
+                        <div className={s.item}><img alt={"LinkedIn"}/></div>
+                        <div className={s.item}><img alt={"Github"}/></div>
+                        <div className={s.item}><img alt={"VK"}/></div>
+                    </div>
+
                 </div>
                     <div className={s.copyRight}>
                 Ⓒ 2023 All right reserved
