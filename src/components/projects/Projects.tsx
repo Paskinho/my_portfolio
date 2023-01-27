ddfb39f7dcb8beac675ea4406cde4e04bb2742c4
paskanov <v.paskanov@gmail.com>
2023-01-27 09:23:12 +0300

    refactoring with Projects.tsx

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,14 +1,20 @@
 import React from "react";
 import s from "./Projects.module.css"
 import sContainer from '../../common/styles/Container.module.css'
+import {Skill} from "../skills/skill/Skill";
 
 
 
 export const Projects = () => {
     return (
-        <div className={s.mainBlock}>
-            <div className={sContainer.container}>
+        <div className={s.skillsBlock}>
+            <div className={`${sContainer.container} ${s.skillsContainer}`}>
                 <h2 className={s.title}>Projects</h2>
+                <div className={s.skills}>
+                    <Skill title={"React"} description={"I learn react"}/>
+                    <Skill title={"JS"} description={"I learn JavaScript"}/>
+                    <Skill title={"TS"} description={"I learn TypeScript"}/>
+                </div>
             </div>
         </div>
     );
