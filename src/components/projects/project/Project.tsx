e0432424c22111303730a232db464e41a0b6b9a8
paskanov <v.paskanov@gmail.com>
2023-01-31 16:37:18 +0300

    some changes with Project.tsx

--- src/components/projects/project/Project.tsx
+++ src/components/projects/project/Project.tsx
@@ -10,11 +10,12 @@ export const Project = (props:projectPropsType) => {
 
     const projectHandler = () => {
        return alert("Later!")
+
     }
 
     return (
         <div className={s.projectBlock}>
-            <div className={s.project}>
+            <div className={s.projectContainer}>
                 <button onClick={projectHandler} className={s.button}>Seen</button>
             </div>
             <h3 className={s.title}>{props.title}</h3>
