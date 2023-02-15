74b1ebbe0990ea5f0f9e14de55924569372ab569
paskanov <v.paskanov@gmail.com>
2023-02-15 17:19:21 +0300

    some changed css with Main.module.css

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -10,7 +10,9 @@ export const Main = () => {
             <div className={sContainer.container}>
             <div className={s.text}>
                 <span>Hello!</span>
+                <div className={s.mainText}>
                 <h1>I am Uladzislau Paskanau</h1>
+                </div>
                 <p>Frontend Developer</p>
             </div>
             <div className={s.photo}>
