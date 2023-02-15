33898ac2084f3a88af28acfa651fc8bd579d6d92
paskanov <v.paskanov@gmail.com>
2023-02-15 17:10:49 +0300

    some changed css with Nav.module.css

--- src/common/components/title/Title.tsx
+++ src/common/components/title/Title.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import s from "./Title.module.css";
+import s from "./Title.module.scss";
 
 
 type TitlePropsType = {
--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -4,10 +4,11 @@ import s from "./Nav.module.css"
  export const Nav = () => {
     return(
 <div className={s.nav}>
-     <a href=''>Home</a>
-     <a href=''>Skills</a>
-     <a href=''>Projects</a>
-     <a href=''>Contacts</a>
+     <a>Home</a>
+     <a>Skills</a>
+     <a>Projects</a>
+     <a>Contacts</a>
+
 </div>
     );
 }
