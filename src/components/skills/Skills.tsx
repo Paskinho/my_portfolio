6153c60de92a99c767a20bad1a435c8cac69e7ae
paskanov <v.paskanov@gmail.com>
2023-02-16 12:19:52 +0300

    some changing with skills

--- src/components/skills/Skills.tsx
+++ src/components/skills/Skills.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import s from "./Skills.module.css"
+import s from "./Skills.module.scss"
 import sContainer from '../../common/styles/Container.module.css'
 import {Skill} from "./skill/Skill";
 import {Title} from "../../common/components/title/Title";
