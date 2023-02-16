c940b35c779fc540b42144e0cf37f2238621356c
paskanov <v.paskanov@gmail.com>
2023-02-16 12:32:53 +0300

    changed css in Footer.module.scss

--- src/components/contacts/Contacts.tsx
+++ src/components/contacts/Contacts.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import s from "./Contacts.module.css"
+import s from "./Contacts.module.scss"
 import sContainer from "../../common/styles/Container.module.css";
 
 export const Contacts = () => {
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import s from "./Footer.module.css"
+import s from "./Footer.module.scss"
 import sContainer from "../../common/styles/Container.module.css";
 
 export const Footer = () => {
