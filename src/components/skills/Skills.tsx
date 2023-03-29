298e90f0c83c9baed8b409b8e68c8b37924d0b9f
paskanov <v.paskanov@gmail.com>
2023-03-29 16:56:06 +0300

    Add images for my skills and refactoring code in Skills.tsx

--- src/components/skills/Skills.tsx
+++ src/components/skills/Skills.tsx
@@ -3,17 +3,33 @@ import s from "./Skills.module.scss"
 import sContainer from '../../common/styles/Container.module.css'
 import {Skill} from "./skill/Skill";
 import {Title} from "../../common/components/title/Title";
-
+import reactImage from "../../assets/images/React.png"
+import jsImage from "../../assets/images/JS.png"
+import typeScriptImage from "../../assets/images/Typescript.png"
 
 export const Skills = () => {
+
+    const ReactImage = {
+        backgroundImage:`url(${reactImage})`
+    }
+
+    const JSImage = {
+        backgroundImage:`url(${jsImage})`
+    }
+
+    const TypeScriptImage = {
+        backgroundImage:`url(${typeScriptImage})`
+    }
+
+
     return (
         <div className={s.skillsBlock}>
             <div className={`${sContainer.container} ${s.skillsContainer}`}>
                 <Title text={'Skills'}/>
                 <div className={s.skills}>
-                <Skill title={"React"} description={"I learn react"}/>
-                <Skill title={"JS"} description={"I learn JavaScript"}/>
-                <Skill title={"TS"} description={"I learn TypeScript"}/>
+                <Skill style={ReactImage} title={"React"} description={"I learn react"}/>
+                <Skill style={JSImage} title={"JS"} description={"I learn JavaScript"}/>
+                <Skill style={TypeScriptImage} title={"TS"} description={"I learn TypeScript"}/>
                 </div>
             </div>
         </div>
