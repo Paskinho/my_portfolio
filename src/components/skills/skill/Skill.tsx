b8bb058fb2097177186703d5b1bec23a6e454d5a
paskanov <v.paskanov@gmail.com>
2023-03-29 16:58:00 +0300

    add icons in skills after review Sveta

--- src/components/skills/skill/Skill.tsx
+++ src/components/skills/skill/Skill.tsx
@@ -4,12 +4,13 @@ import s from "./Skill.module.scss"
 type skillPropsType = {
     title: string
     description: string
+    style: any // уточнить тип
 }
 
 export const Skill = (props:skillPropsType) => {
     return (
         <div className={s.skillBlock}>
-            <div className={s.icon}></div>
+            <div className={s.icon} style={props.style}></div>
             <h3>{props.title}</h3>
             <span className={s.description}>
                 {props.description}
