43ac8c50c8cdc6bbfd4f5b4fb0eb7c5cef37c0b7
paskanov <v.paskanov@gmail.com>
2023-04-05 15:51:22 +0300

    Changed css in navMenu

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -4,10 +4,10 @@ import s from "./Nav.module.scss"
 export const Nav = () => {
     return (
         <div className={s.nav}>
-            <a>Home</a>
-            <a>Projects</a>
-            <a>Skills</a>
-            <a>Contacts</a>
+            <a className={s.navLink}>Home</a>
+            <a className={s.navLink}>Projects</a>
+            <a className={s.navLink}>Skills</a>
+            <a className={s.navLink}>Contacts</a>
         </div>
     );
 }
