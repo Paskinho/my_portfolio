95af0cf635c1898f8ea6daba2964420cdb845360
paskanov <v.paskanov@gmail.com>
2023-04-11 11:56:13 +0300

    3 lesson. Some refactoring with navigation

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import s from "./Nav.module.scss"
 // import {NavLink} from "react-router-dom";
-// import {Link} from 'react-scroll'
+import {Link} from 'react-scroll'
 
 
 export const Nav = () => {
