38840408474cf731e106e2cc43a25c45caf88413
paskanov <v.paskanov@gmail.com>
2023-07-18 12:54:59 +0300

    3 lesson.Some changes with backend (nodemailer)

--- src/index.tsx
+++ src/index.tsx
@@ -4,47 +4,7 @@ import './index.css';
 import App from './app/App';
 import reportWebVitals from './reportWebVitals';
 import {Provider} from "react-redux";
-import {store} from "./app/store";
-import express from 'express';
-
-//
-// // const express = require('express')
-// const nodemailer = require('nodemailer')
-//
-// const app = express();
-//
-//
-// let transporter = nodemailer.createTransport({
-//     service: 'gmail',
-//     // host: "smtp.ethereal.email",
-//     // port: 587,
-//     // secure: false, // true for 465, false for other ports
-//     auth: {
-//         user: 'v.paskanov@gmail.com', // generated ethereal user
-//         pass: '', // generated ethereal password
-//     },
-// });
-//
-// // app.get('/', (req, res)=> {
-// //     res.send('hello world')
-// // })
-//
-// app.get('/sendMessage', async (req: any,res: any)=> {
-//     let info = await transporter.sendMail({
-//         from: "Pask Message",
-//         to: "v.paskanov@gmail.com",
-//         subject: 'Testing gmail',
-//         //text: "Hello, bro!",
-//         html: 'Hello, bro!'
-//     })
-//     res.send('yoyoyo')
-// })
-//
-// app.listen(3010,()=> {
-//     console.log('3010 port')
-// })
-
-
+import {store} from "./app/store";;
 
 
 const root = ReactDOM.createRoot(
